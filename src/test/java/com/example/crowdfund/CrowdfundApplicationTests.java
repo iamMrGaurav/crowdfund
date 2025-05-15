a6fba9459f4279ceaa9a3ced4b08c940b8bbe4aa
Gaurav Poudel <gaurair@Gauravs-MacBook-Air.local>
2025-05-15 23:53:34 +0530

    Crowdfund project setup

---
+++ src/test/java/com/example/crowdfund/CrowdfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crowdfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
