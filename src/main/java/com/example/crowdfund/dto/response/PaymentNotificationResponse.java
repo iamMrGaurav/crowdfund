7578fa32152a8aee91a820594b29a75e4a3e746f
Gaurav Poudel <gaurair@Gauravs-MacBook-Air.local>
2025-07-20 14:05:07 +0530

    Consumer Config updated

--- src/main/java/com/example/crowdfund/dto/response/PaymentNotificationResponse.java
+++ src/main/java/com/example/crowdfund/dto/response/PaymentNotificationResponse.java
@@ -3,11 +3,13 @@ package com.example.crowdfund.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentNotificationResponse {
     String donorName;
     BigDecimal amount;
